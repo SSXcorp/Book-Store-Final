0938d1abddcf0c2e6afa290bab915891f9bf46d3
ssx_corp <mr.dimov77@gmail.com>
2025-03-21 19:31:40 +0200

    Lombok annotations replaced with @Data

--- BookStoreProd/src/main/java/mate/academy/bookstoreprod/dto/BookDto.java
+++ BookStoreProd/src/main/java/mate/academy/bookstoreprod/dto/BookDto.java
@@ -1,15 +1,9 @@
 package mate.academy.bookstoreprod.dto;
 
 import java.math.BigDecimal;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Data;
 
-@Getter
-@Setter
-@ToString
-@RequiredArgsConstructor
+@Data
 public class BookDto {
     private int id;
     private String title;
