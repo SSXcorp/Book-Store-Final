8ab843a5ea8f8f3c6397263882f96281ab86445a
ssx_corp <mr.dimov77@gmail.com>
2025-03-21 19:31:19 +0200

    Deleted unused constructor

--- BookStoreProd/src/main/java/mate/academy/bookstoreprod/exception/EntityNotFoundException.java
+++ BookStoreProd/src/main/java/mate/academy/bookstoreprod/exception/EntityNotFoundException.java
@@ -1,13 +1,8 @@
 package mate.academy.bookstoreprod.exception;
 
 public class EntityNotFoundException extends RuntimeException {
-
     public EntityNotFoundException(String message) {
         super(message);
-    }
-
-    public EntityNotFoundException(Throwable cause) {
-        super(cause);
     }
 
     public EntityNotFoundException(String message, Throwable cause) {
