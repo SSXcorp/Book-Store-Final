21d602916dae3b18f78d55f94cacfd086b86a0f6
ssx_corp <mr.dimov77@gmail.com>
2025-03-21 18:25:33 +0200

    Custom exception EntityNotFoundException created

---
+++ BookStoreProd/src/main/java/mate/academy/bookstoreprod/exception/EntityNotFoundException.java
@@ -0,0 +1,16 @@
+package mate.academy.bookstoreprod.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+    public EntityNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public EntityNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
