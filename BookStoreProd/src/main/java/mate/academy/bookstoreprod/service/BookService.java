17e1bd6d9798ba76bbde9315390eb04c066444a6
ssx_corp <mr.dimov77@gmail.com>
2025-03-25 20:41:25 +0200

    Swagger implementation

--- BookStoreProd/src/main/java/mate/academy/bookstoreprod/service/BookService.java
+++ BookStoreProd/src/main/java/mate/academy/bookstoreprod/service/BookService.java
@@ -4,10 +4,11 @@ import java.util.List;
 import mate.academy.bookstoreprod.dto.BookDto;
 import mate.academy.bookstoreprod.dto.BookSearchParametersDto;
 import mate.academy.bookstoreprod.dto.CreateBookRequestDto;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto findById(Long id);
 
