dc093cba801b1fe3fa29694b3b97d4a6fe64a079
ssx_corp <mr.dimov77@gmail.com>
2025-03-25 17:30:25 +0200

    Custom exceptions and global exception handler added : GlobalExceptionHandler EntityAlreadyExistsException and EntityNotFoundException

---
+++ BookStoreProd/src/main/java/mate/academy/bookstoreprod/exception/handler/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package mate.academy.bookstoreprod.exception.handler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.bookstoreprod.exception.EntityAlreadyExistsException;
+import mate.academy.bookstoreprod.exception.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAllExceptions(Exception ex) {
+        return new ResponseEntity<>("An unexpected error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<String> handleNullPointerExceptions(NullPointerException ex) {
+        return new ResponseEntity<>("Null pointer exception occurred", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundExceptions(EntityNotFoundException ex) {
+        return new ResponseEntity<>("Entity not found exception occurred", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult()
+                .getAllErrors().stream()
+                .map(objectError -> {
+                    String fieldName = ((FieldError) objectError).getField();
+                    String message = objectError.getDefaultMessage();
+                    return "Field '" + fieldName + "' " + message;
+                })
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EntityAlreadyExistsException.class)
+    public ResponseEntity<String> handleEntityAlreadyExistsExceptions(EntityAlreadyExistsException ex) {
+        return new ResponseEntity<>("Entity already exists exception occurred", HttpStatus.BAD_REQUEST);
+    }
+}
