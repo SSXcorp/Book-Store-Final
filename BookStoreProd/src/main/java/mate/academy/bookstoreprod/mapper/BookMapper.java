acf9884de96377da4fdda0d423463ca8b73f60de
ssx_corp <mr.dimov77@gmail.com>
2025-03-31 21:06:53 +0300

    Package changed

--- BookStoreProd/src/main/java/mate/academy/bookstoreprod/mapper/BookMapper.java
+++ BookStoreProd/src/main/java/mate/academy/bookstoreprod/mapper/BookMapper.java
@@ -1,8 +1,8 @@
 package mate.academy.bookstoreprod.mapper;
 
 import mate.academy.bookstoreprod.config.MapperConfig;
-import mate.academy.bookstoreprod.dto.BookDto;
-import mate.academy.bookstoreprod.dto.CreateBookRequestDto;
+import mate.academy.bookstoreprod.dto.book.BookDto;
+import mate.academy.bookstoreprod.dto.book.CreateBookRequestDto;
 import mate.academy.bookstoreprod.model.Book;
 import org.mapstruct.Mapper;
 
