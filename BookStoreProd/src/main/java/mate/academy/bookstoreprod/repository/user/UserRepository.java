4bae7ba99082ba465ac96be021a9570121f7aeae
ssx_corp <mr.dimov77@gmail.com>
2025-04-09 18:31:27 +0300

    UserDetails and UserDetailsService added. Security config added

--- BookStoreProd/src/main/java/mate/academy/bookstoreprod/repository/user/UserRepository.java
+++ BookStoreProd/src/main/java/mate/academy/bookstoreprod/repository/user/UserRepository.java
@@ -1,10 +1,16 @@
 package mate.academy.bookstoreprod.repository.user;
 
+import java.util.Optional;
 import mate.academy.bookstoreprod.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+
+    @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.email = :email")
+    Optional<User> findByEmail(@Param("email") String email);
 }
