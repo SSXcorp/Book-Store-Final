d8f7dcfa78bfdd81ee8c90b517f6d97aef66f7ae
ssx_corp <mr.dimov77@gmail.com>
2025-04-09 20:35:01 +0300

    Type of exception changed from UsernameNotFoundException to EntityNotFoundException

--- BookStoreProd/src/main/java/mate/academy/bookstoreprod/security/CustomUserDetailsService.java
+++ BookStoreProd/src/main/java/mate/academy/bookstoreprod/security/CustomUserDetailsService.java
@@ -1,10 +1,10 @@
 package mate.academy.bookstoreprod.security;
 
 import lombok.RequiredArgsConstructor;
+import mate.academy.bookstoreprod.exception.EntityNotFoundException;
 import mate.academy.bookstoreprod.repository.user.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,8 +13,8 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) {
         return userRepository.findByEmail(email).orElseThrow(() ->
-                new UsernameNotFoundException("Cannot find user with email: " + email));
+                new EntityNotFoundException("Cannot find user with email: " + email));
     }
 }
