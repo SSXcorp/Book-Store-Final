e8a63871bb0b2ad235b7dac9406bb043b886d49b
ssx_corp <mr.dimov77@gmail.com>
2025-04-09 20:33:27 +0300

    EntityGraph used against Join Fetch

--- BookStoreProd/src/main/java/mate/academy/bookstoreprod/repository/user/UserRepository.java
+++ BookStoreProd/src/main/java/mate/academy/bookstoreprod/repository/user/UserRepository.java
@@ -2,8 +2,8 @@ package mate.academy.bookstoreprod.repository.user;
 
 import java.util.Optional;
 import mate.academy.bookstoreprod.model.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +11,6 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
-    @Query("SELECT u FROM User u JOIN FETCH u.roles WHERE u.email = :email")
+    @EntityGraph(attributePaths = "roles")
     Optional<User> findByEmail(@Param("email") String email);
 }
