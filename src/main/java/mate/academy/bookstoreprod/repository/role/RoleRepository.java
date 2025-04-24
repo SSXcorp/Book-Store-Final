17595181f25faeb5009f44484541af23c629e5eb
ssx_corp <mr.dimov77@gmail.com>
2025-04-24 11:59:23 +0300

    Minor fix

--- src/main/java/mate/academy/bookstoreprod/repository/role/RoleRepository.java
+++ src/main/java/mate/academy/bookstoreprod/repository/role/RoleRepository.java
@@ -4,7 +4,9 @@ import java.util.Optional;
 import mate.academy.bookstoreprod.model.Role;
 import mate.academy.bookstoreprod.model.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(RoleName name);
 }
