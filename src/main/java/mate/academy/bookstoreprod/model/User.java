cc9474cb26337c9523eadeb44618a2339f346bf9
ssx_corp <mr.dimov77@gmail.com>
2025-05-02 17:09:33 +0300

    User entity: shoppingCart added

--- src/main/java/mate/academy/bookstoreprod/model/User.java
+++ src/main/java/mate/academy/bookstoreprod/model/User.java
@@ -1,13 +1,17 @@
 package mate.academy.bookstoreprod.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.util.Collection;
 import java.util.HashSet;
@@ -45,6 +49,10 @@ public class User implements UserDetails {
     private Set<Role> roles = new HashSet<>();
     private String shippingAddress;
     private boolean isDeleted;
+
+    @JsonBackReference
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private ShoppingCart shoppingCart;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
