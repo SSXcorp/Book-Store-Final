26ab1cef376695e740540e02570717514e7d30c9
ssx_corp <mr.dimov77@gmail.com>
2025-05-03 21:31:36 +0300

    FetchType Lazy added for book and ShoppingCart

--- src/main/java/mate/academy/bookstoreprod/model/CartItem.java
+++ src/main/java/mate/academy/bookstoreprod/model/CartItem.java
@@ -1,7 +1,7 @@
 package mate.academy.bookstoreprod.model;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -22,14 +22,13 @@ public class CartItem {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(optional = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "shopping_cart_id")
     private ShoppingCart shoppingCart;
 
-    @ManyToOne(optional = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "book_id")
     private Book book;
 
-    @Column(nullable = false)
     private int quantity;
 }
