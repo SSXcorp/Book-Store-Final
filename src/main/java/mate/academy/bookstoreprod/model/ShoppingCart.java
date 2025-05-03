1c8bdc1c6a27991274c37d1f0b51e4d6219a6a2b
ssx_corp <mr.dimov77@gmail.com>
2025-05-03 21:34:11 +0300

    MapsId, FetchType.LAZY for user added to ShoppingCart.

--- src/main/java/mate/academy/bookstoreprod/model/ShoppingCart.java
+++ src/main/java/mate/academy/bookstoreprod/model/ShoppingCart.java
@@ -1,12 +1,11 @@
 package mate.academy.bookstoreprod.model;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -27,12 +26,11 @@ import org.hibernate.annotations.SQLRestriction;
 @SQLRestriction("is_deleted = FALSE")
 public class ShoppingCart {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne(optional = false)
+    @MapsId
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false, unique = true)
-    @JsonManagedReference
     private User user;
 
     @OneToMany(mappedBy = "shoppingCart", cascade = CascadeType.ALL, orphanRemoval = true)
