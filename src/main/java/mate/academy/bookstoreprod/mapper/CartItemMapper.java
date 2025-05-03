cd3321c3192491b45a64d273884fd3c60fbb2f7d
ssx_corp <mr.dimov77@gmail.com>
2025-05-03 21:31:04 +0300

    Mappers for CartItem and ShoppingCart separated

---
+++ src/main/java/mate/academy/bookstoreprod/mapper/CartItemMapper.java
@@ -0,0 +1,15 @@
+package mate.academy.bookstoreprod.mapper;
+
+import mate.academy.bookstoreprod.config.MapperConfig;
+import mate.academy.bookstoreprod.dto.shoppingcart.CartItemResponseDto;
+import mate.academy.bookstoreprod.model.CartItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(config = MapperConfig.class)
+public interface CartItemMapper {
+
+    @Mapping(source = "book.id", target = "bookId")
+    @Mapping(source = "book.title", target = "bookTitle")
+    CartItemResponseDto toDto(CartItem cartItem);
+}
--- src/main/java/mate/academy/bookstoreprod/mapper/ShoppingCartMapper.java
+++ src/main/java/mate/academy/bookstoreprod/mapper/ShoppingCartMapper.java
@@ -1,22 +1,14 @@
 package mate.academy.bookstoreprod.mapper;
 
 import mate.academy.bookstoreprod.config.MapperConfig;
-import mate.academy.bookstoreprod.dto.shoppingcart.CartItemResponseDto;
 import mate.academy.bookstoreprod.dto.shoppingcart.ShoppingCartResponseDto;
-import mate.academy.bookstoreprod.model.CartItem;
 import mate.academy.bookstoreprod.model.ShoppingCart;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(config = MapperConfig.class)
+@Mapper(config = MapperConfig.class, uses = CartItemMapper.class)
 public interface ShoppingCartMapper {
-
     @Mapping(source = "user.id", target = "userId")
     @Mapping(source = "cartItems", target = "cartItems")
     ShoppingCartResponseDto toDto(ShoppingCart cart);
-
-    @Mapping(source = "book.id", target = "bookId")
-    @Mapping(source = "book.title", target = "bookTitle")
-    CartItemResponseDto toDto(CartItem cartItem);
-
 }
