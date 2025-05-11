9564819ec34ba68b337bd692ad50f0a3a42cae7b
ssx_corp <mr.dimov77@gmail.com>
2025-05-11 19:09:11 +0300

    Checkstyle fix

--- src/main/java/mate/academy/bookstoreprod/repository/shoppingcart/ShoppingCartRepository.java
+++ src/main/java/mate/academy/bookstoreprod/repository/shoppingcart/ShoppingCartRepository.java
@@ -1,8 +1,6 @@
 package mate.academy.bookstoreprod.repository.shoppingcart;
 
 import java.util.Optional;
-import java.util.Set;
-import mate.academy.bookstoreprod.model.CartItem;
 import mate.academy.bookstoreprod.model.ShoppingCart;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
