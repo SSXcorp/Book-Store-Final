44fd75e931b77d37f3bfaaf659261c91c80abb25
ssx_corp <mr.dimov77@gmail.com>
2025-05-20 09:46:54 +0300

    setUp fix

--- src/test/java/mate/academy/bookstoreprod/controller/CategoryControllerTest.java
+++ src/test/java/mate/academy/bookstoreprod/controller/CategoryControllerTest.java
@@ -13,7 +13,6 @@ import mate.academy.bookstoreprod.dto.category.CreateCategoryDto;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -24,7 +23,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CategoryControllerTest {
     protected static MockMvc mockMvc;
 
@@ -41,7 +39,7 @@ public class CategoryControllerTest {
     private ObjectMapper objectMapper;
 
     @BeforeAll
-    void setUp(@Autowired WebApplicationContext context) {
+    static void setUp(@Autowired WebApplicationContext context) {
         mockMvc = MockMvcBuilders
                 .webAppContextSetup(context)
                 .apply(springSecurity())
